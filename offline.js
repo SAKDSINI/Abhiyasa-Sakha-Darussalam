46ae802876e9075bed581ebb69d1fe8c5a20dbea
SAKDSINI <151989023+SAKDSINI@users.noreply.github.com>
2023-11-25 13:39:25 +0700

    Add files via upload
    Robotic DA

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1700894144,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/9patch.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/pngtreestraightroadpngimage_6262171removebgpreview-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/4269png_860removebgpreview-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
